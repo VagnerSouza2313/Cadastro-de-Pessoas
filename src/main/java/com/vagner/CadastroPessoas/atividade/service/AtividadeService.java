045f45de719c5b1a346681d07d1abb4fa3ea9d16
Vagner Souza <juninfps0@gmail.com>
2026-05-27 17:02:38 -0300

    feat: feito crud completo mas falta testar e chamar no controller

---
+++ src/main/java/com/vagner/CadastroPessoas/atividade/service/AtividadeService.java
@@ -0,0 +1,64 @@
+package com.vagner.CadastroPessoas.atividade.service;
+
+import com.vagner.CadastroPessoas.atividade.domain.Atividade;
+import com.vagner.CadastroPessoas.atividade.dto.AtividadeDto;
+import com.vagner.CadastroPessoas.atividade.mappers.AtividadeDtoMapper;
+import com.vagner.CadastroPessoas.atividade.repository.AtividadeRepository;
+import com.vagner.CadastroPessoas.pessoa.domain.Pessoa;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class AtividadeService {
+
+    AtividadeRepository atividadeRepository;
+    AtividadeDtoMapper atividadeDtoMapper;
+
+    public AtividadeService(AtividadeRepository atividadeRepository, AtividadeDtoMapper atividadeDtoMapper) {
+        this.atividadeRepository = atividadeRepository;
+        this.atividadeDtoMapper = atividadeDtoMapper;
+    }
+
+    //Create person
+    public AtividadeDto create(AtividadeDto atividadeDto){
+        Atividade atividade = atividadeDtoMapper.toDomain(atividadeDto);
+        atividadeRepository.save(atividade);
+        return  atividadeDtoMapper.toDto(atividade);
+    }
+
+    //Read person
+    public List<AtividadeDto> read(){
+        List<Atividade> atividadeList = atividadeRepository.findAll();
+        return atividadeList.stream().map(atividadeDtoMapper::toDto).collect(Collectors.toList());
+    }
+
+    //Read by id
+    public AtividadeDto readId(Long id){
+        Optional<Atividade> atividade = atividadeRepository.findById(id);
+        return atividade.map(atividadeDtoMapper::toDto).orElse(null); //fazer exceções
+    }
+
+    //Update person
+    public AtividadeDto delete(Long id, AtividadeDto atividadeDto){
+        Optional<Atividade> atividade = atividadeRepository.findById(id);
+        if (atividade.isPresent()){
+            Atividade atividadeUpdate = atividade.get();
+            atividadeUpdate.setDescricao(atividadeDto.getDescricao());
+            atividadeUpdate.setGrauAtividade(atividadeDto.getGrauAtividade());
+            Atividade atividadeSave = atividadeRepository.save(atividadeUpdate);
+            return atividadeDtoMapper.toDto(atividadeSave);
+        }
+        else {
+            return null;
+        }
+    }
+
+    //Delete person
+    public void delete(Long id){
+        atividadeRepository.deleteById(id);
+    }
+
+}
