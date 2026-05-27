a446ebc4a3410190be36bbcfe7b4411579dfb7c2
Vagner Souza <juninfps0@gmail.com>
2026-05-27 17:01:38 -0300

    fix: arruma relação de atividade e pessoa

--- src/main/java/com/vagner/CadastroPessoas/atividade/domain/Atividade.java
+++ src/main/java/com/vagner/CadastroPessoas/atividade/domain/Atividade.java
@@ -1,10 +1,15 @@
 package com.vagner.CadastroPessoas.atividade.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.vagner.CadastroPessoas.atividade.enums.GrauAtividade;
+import com.vagner.CadastroPessoas.pessoa.domain.Pessoa;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,5 +23,10 @@ public class Atividade{
     private Long id;
     private String descricao;
     private GrauAtividade grauAtividade;
+
+    @OneToMany(mappedBy = "atividade")
+    @JsonIgnore
+    private List<Pessoa> pessoas = new ArrayList<>();
+
 
 }
