f7a10f1699347fc0f8c3e6f27ca9ff89052c6eae
Vagner Souza <juninfps0@gmail.com>
2026-06-09 22:21:01 -0300

    feat: controlador geral de exceções com a exceção de Id nao encontrado

---
+++ src/main/java/com/vagner/CadastroPessoas/atividade/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.vagner.CadastroPessoas.atividade.controller.exceptions;
+
+import com.vagner.CadastroPessoas.atividade.service.exceptions.IdNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    public ResponseEntity<StandardError> idNotFound(IdNotFoundException e, HttpServletRequest request){
+        StandardError error = new StandardError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setError("Id not found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+}
