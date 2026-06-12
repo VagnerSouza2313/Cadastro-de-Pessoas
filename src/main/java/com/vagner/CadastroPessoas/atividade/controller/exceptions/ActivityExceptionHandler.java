cd804b1a14ea6e200690c155a4dc617350b4903f
Vagner Souza <juninfps0@gmail.com>
2026-06-11 23:45:44 -0300

    fix: apenas adiciona a anotaçao de ExceptionHandler e a classe nela que estava faltando

--- src/main/java/com/vagner/CadastroPessoas/atividade/controller/exceptions/ActivityExceptionHandler.java
+++ src/main/java/com/vagner/CadastroPessoas/atividade/controller/exceptions/ActivityExceptionHandler.java
@@ -5,12 +5,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class ActivityExceptionHandler {
 
+    @ExceptionHandler(IdNotFoundException.class)
     public ResponseEntity<StandardError> idNotFound(IdNotFoundException e, HttpServletRequest request){
         StandardError error = new StandardError();
         error.setTimestamp(Instant.now());
